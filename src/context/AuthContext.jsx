fdc8a5e9617a1fa4b5bbd2b7d437e9f496f1c17f
phiona-dev <phionadev@gmail.com>
2026-01-30 12:21:00 +0300

    localstorage peristance

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,14 +3,19 @@ import React, { createContext, useState, useContext } from 'react'
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(() => {
+        const storedUser = localStorage.getItem("user")
+        return storedUser ? JSON.parse(storedUser) : null
+    })
 
-    const login = (loggedUser) => {
-        setUser(loggedUser)
+    const login = (userData) => {
+        setUser(userData)
+        localStorage.setItem("user", JSON.stringify(userData))
     }
 
     const logout = () => {
         setUser(null)
+        localStorage.removeItem("user")
     }
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
