1ae2207bf020a86543981ebaf7d3de9f96355a8f
phiona-dev <phionadev@gmail.com>
2026-02-17 23:32:49 +0300

    signup endpoint

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,41 @@
 const router = require("express").Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt")
+
+const saltRounds = 10;
 
 router.post("/signup", async (req, res) => {
-    console.log("Signup route")
-    console.log(req.body)
-    res.json({ message: "Route working" })
+    
+    try{
+        const { email, password, name } = req.body;
+        if(!email || !password){
+            return res.status(400).json({ message: "Email and password required" })
+        }
+
+        const existingUser = await User.findOne({ email })
+
+        if (existingUser){
+            return res.status(400).json({ message: "Email already registered"})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, saltRounds)
+
+        const newUser = new User({
+            email,
+            password: hashedPassword,
+            name,
+            createdAt: new Date()
+        })
+
+        await newUser.save();
+
+        return res.status(201).json({ message: "User created successfully"})
+
+    }catch (error){
+        console.error(error);
+        return res.status(500).json({ message: "Server error"})
+    }
+    
 
 })
 
