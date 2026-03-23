013ddad4e21bb2d70dbbc8e245d0c01941443cc6
phiona-dev <phionadev@gmail.com>
2026-03-23 23:31:42 +0300

    refined productschema

--- backend/src/models/product.js
+++ backend/src/models/product.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    productName: String,
-    brand: String,
-    ingredientList: [],
-    createdAt: Date
-})
+    productName: {type: String, required: true, trim: true },
+    brand: {type: String, trim: true},
+    categoryId: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true},
+    ingredientList: [{ type: mongoose.Schema.Types.ObjectId, ref: "Ingredient" }]
+}, { timestamps: true})
 
 const Product = mongoose.model("Product", productSchema)
 module.exports = Product
