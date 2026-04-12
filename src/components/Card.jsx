e281ef7529215e7936284b51c14223b3efe98196
phiona-dev <phionadev@gmail.com>
2026-04-12 20:40:57 +0300

    product-img

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import "./Card.css"
+import defaultImg from "../assets/default.jpg"
 
 const Card = ({ title, brand, category }) => {
   return (
     <div className='product-card'>
+      <div>
+        <img src={defaultImg} alt={title} className='product-img' />
+      </div>
         <h2 className='product-title'>{title}</h2>
         <h4 className='product-brand'>{brand || "Unkown brand"}</h4>
         <p className='product-category'>{category || "Uncategorized"}</p>
